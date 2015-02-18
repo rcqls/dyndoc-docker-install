b1480624a954cd37bdaf603c0435614e52f8dd26
R cqls <rdrouilh@gmail.com>
2015-02-18 11:29:53 +0100

    install updated

--- install-dyndoc-docker-client.sh
+++ install-dyndoc-docker-client.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
+if [ "$1" = "" ]; then
+	DYNDOC_DOCKER_HOME=~/dyndoc-docker
+else
+	DYNDOC_DOCKER_HOME=$1
+fi
 
-DYNDOC_DOCKER_HOME=~/dyndoc-docker2
-mkdir -p ${DYNDOC_DOCKER_HOME}/etc
+mkdir -p ${DYNDOC_DOCKER_HOME}
 mkdir -p ${DYNDOC_DOCKER_HOME}/library/{R,dyndoc,ruby}
 mkdir -p ${DYNDOC_DOCKER_HOME}/proj/rooms
 
 mkdir -p ${DYNDOC_DOCKER_HOME}/.install
 cd ${DYNDOC_DOCKER_HOME}/.install
-git clone http://github.com/rcqls/dyndoc-docker
+git clone https://github.com/rcqls/dyndoc-docker-install.git
+cd ${DYNDOC_DOCKER_HOME}
+ln -sf .install/dyndoc-docker-install/client/etc
 
+echo "To finalize your installation, add in your .bash_profile (or equivalent):"
+echo "   export DYNDOC_DOCKER_HOME=${DYNDOC_DOCKER_HOME}"
+echo "   . \$DYNDOC_DOCKER_HOME/etc/init/ddyn.sh"
 
