7db9fa3d121dbb0087751fb3498368ddb8687b62
R cqls <rdrouilh@gmail.com>
2015-02-18 14:07:05 +0100

    cp instead of ln

--- install-dyndoc-docker-client.sh
+++ install-dyndoc-docker-client.sh
@@ -13,9 +13,9 @@ mkdir -p ${DYNDOC_DOCKER_HOME}/.install
 cd ${DYNDOC_DOCKER_HOME}/.install
 git clone https://github.com/rcqls/dyndoc-docker-install.git
 cd ${DYNDOC_DOCKER_HOME}
-ln -sf .install/dyndoc-docker-install/client/etc
+cp -r .install/dyndoc-docker-install/client/etc .
 
 echo "To finalize your installation, add in your .bash_profile (or equivalent):"
-echo "   export DYNDOC_DOCKER_HOME=${DYNDOC_DOCKER_HOME}"
-echo "   . \$DYNDOC_DOCKER_HOME/etc/init/ddyn.sh"
+echo "export DYNDOC_DOCKER_HOME=${DYNDOC_DOCKER_HOME}"
+echo ". \$DYNDOC_DOCKER_HOME/etc/init/ddyn.sh"
 
